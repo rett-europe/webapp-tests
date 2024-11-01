65ca61310f67afb11fb9f823bee1fd8a30bf469c
Pedro Rocha <pedrojrocha@outlook.com>
2024-11-01 22:45:28 +0100

    Update gh-pages with new assets

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "415706298"
+    serviceWorkerVersion: "3055203821"
   }
 });
