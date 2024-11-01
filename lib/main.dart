1e195beac4e501a5ee8e142650e09042ed7c48d9
Pedro Rocha <pedrojrocha@outlook.com>
2024-11-01 15:54:42 +0100

    Add example app integrated with auth0

--- lib/main.dart
+++ lib/main.dart
@@ -1,97 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'example_app.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+
+   await dotenv.load();
+
+  runApp(const ExampleApp());
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Dummy Login App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: LoginPage(),
-    );
-  }
-}
-
-class LoginPage extends StatefulWidget {
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  final TextEditingController _usernameController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-  bool _isLoggingIn = false;
-
-  void _login() {
-    setState(() {
-      _isLoggingIn = true;
-    });
-
-    // Simulate a delay to show "logging in" status
-    Future.delayed(Duration(seconds: 2), () {
-      setState(() {
-        _isLoggingIn = false;
-      });
-      showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-          title: Text("Login Status"),
-          content: Text("Login ongoing..."),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text("OK"),
-            ),
-          ],
-        ),
-      );
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Dummy Login Page"),
-      ),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextField(
-                controller: _usernameController,
-                decoration: InputDecoration(
-                  labelText: "Username",
-                ),
-              ),
-              SizedBox(height: 16.0),
-              TextField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: "Password",
-                ),
-              ),
-              SizedBox(height: 32.0),
-              _isLoggingIn
-                  ? CircularProgressIndicator()
-                  : ElevatedButton(
-                      onPressed: _login,
-                      child: Text("Login"),
-                    ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
