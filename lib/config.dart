49fb56e9b27c673910d46295b82adcaecc2a51b4
Pedro Rocha <pedrojrocha@outlook.com>
2024-11-01 16:09:04 +0100

    New deployment of webapp

---
+++ lib/config.dart
@@ -0,0 +1,4 @@
+const String auth0Domain = 'rett-europe.eu.auth0.com'; 
+const String auth0ClientId = 'nWmcJUIgdJarOse7jFqcjJ7K0JmyP7wf'; 
+const String redirectUri = 'https://rett-europe.github.io/webapp_tests/callback';
+const String auth0CustomScheme = 'com.auth0.sample';
