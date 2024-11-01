d35a71edafeffa38cc26e1b4809b7dc36e5d9d15
Pedro Rocha <pedrojrocha@outlook.com>
2024-11-01 16:05:19 +0100

    Update to deploy in github pages

--- lib/example_app.dart
+++ lib/example_app.dart
@@ -2,9 +2,10 @@ import 'package:auth0_flutter/auth0_flutter.dart';
 import 'package:auth0_flutter/auth0_flutter_web.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+//import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'constants.dart';
+import 'config.dart';
 import 'hero.dart';
 import 'user.dart';
 
@@ -26,9 +27,9 @@ class _ExampleAppState extends State<ExampleApp> {
   void initState() {
     super.initState();
     auth0 = widget.auth0 ??
-        Auth0(dotenv.env['AUTH0_DOMAIN']!, dotenv.env['AUTH0_CLIENT_ID']!);
+        Auth0(auth0Domain, auth0ClientId);
     auth0Web =
-        Auth0Web(dotenv.env['AUTH0_DOMAIN']!, dotenv.env['AUTH0_CLIENT_ID']!);
+        Auth0Web(auth0Domain, auth0ClientId);
 
     if (kIsWeb) {
       auth0Web.onLoad().then((final credentials) => setState(() {
@@ -40,11 +41,11 @@ class _ExampleAppState extends State<ExampleApp> {
   Future<void> login() async {
     try {
       if (kIsWeb) {
-        return auth0Web.loginWithRedirect(redirectUrl: 'http://localhost:3000');
+        return auth0Web.loginWithRedirect(redirectUrl: redirectUri);
       }
 
       var credentials = await auth0
-          .webAuthentication(scheme: dotenv.env['AUTH0_CUSTOM_SCHEME'])
+          .webAuthentication(scheme: auth0CustomScheme)
           // Use a Universal Link callback URL on iOS 17.4+ / macOS 14.4+
           // useHTTPS is ignored on Android
           .login(useHTTPS: true);
@@ -60,10 +61,10 @@ class _ExampleAppState extends State<ExampleApp> {
   Future<void> logout() async {
     try {
       if (kIsWeb) {
-        await auth0Web.logout(returnToUrl: 'http://localhost:3000');
+        await auth0Web.logout(returnToUrl: redirectUri);
       } else {
         await auth0
-            .webAuthentication(scheme: dotenv.env['AUTH0_CUSTOM_SCHEME'])
+            .webAuthentication(scheme: auth0CustomScheme)
             // Use a Universal Link logout URL on iOS 17.4+ / macOS 14.4+
             // useHTTPS is ignored on Android
             .logout(useHTTPS: true);
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+//import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'example_app.dart';
 
 void main() async {
 
-   await dotenv.load();
+  // await dotenv.load();
 
   runApp(const ExampleApp());
 }
